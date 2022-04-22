2692f2579f7185b61cb7dc37967d34b8ee8d81a9
Sergey <mulichev@gmail.com>
2022-04-22 12:39:48 +0300

    Reworked matrix class. Now each matrix instance contain 2-dimensional elements list. With each element can be 3 operation add, multiply(by const or another matrix instance)

--- MatrixOperations/src/main/kotlin/Matrix.kt
+++ MatrixOperations/src/main/kotlin/Matrix.kt
@@ -1,233 +1,140 @@
-object Matrix {
-    private const val lineFirst = " first"
-    private const val lineSecond = " second"
+data class Matrix(var rows: Int, var columns: Int) {
+    enum class Type {
+        Int,
+        Double
+    }
 
-    fun addMatrices(): Boolean {
-        val rawMatrix1 = readMatrix(lineFirst)
-        val rawMatrix2 = readMatrix(lineSecond)
+    private var type = Type.Double
+    var elements: MutableList<MutableList<Double>> = mutableListOf()
+        set(value) {
+            if (value.isEmpty()) throw IllegalStateException("Matrix can't be empty")
+            if (value.size != rows || value.first().size != columns)
+                throw IllegalStateException("Matrix actual size != input size")
+            field = value
+            type = determineMatrixType(value)
+        }
 
-        if (!checkMatrixSizes(
-                rawMatrix1.size,
-                rawMatrix1.first().size,
-                rawMatrix2.size,
-                rawMatrix2.first().size,
+    companion object {
+        private const val lineFirst = " first"
+        private const val lineSecond = " second"
+
+        private fun readMatrix(name: String = ""): Matrix {
+            try {
+                print("Enter size of$name matrix: ")
+                val (rows, columns) = readln().split(" ").map { it.toInt() }
+                println("Enter$name matrix:")
+                val stringMatrix = List(rows) { readln().split(" ") }
+                val matrix = Matrix(rows, columns)
+                matrix.elements = stringMatrix.map { row ->
+                    row.map { it.toDouble() }.toMutableList()
+                }.toMutableList()
+                return matrix
+            } catch (e: NumberFormatException) {
+                throw IllegalStateException("Matrix element or matrix sizes isn't Int nor Double")
+            }
+        }
+
+        fun multiplyMatrices() {
+            val matrix1 = readMatrix(lineFirst)
+            val matrix2 = readMatrix(lineSecond)
+
+            printMatrix(matrix1.multiply(matrix2))
+        }
+
+        fun sumMatrices() {
+            val matrix1 = readMatrix(lineFirst)
+            val matrix2 = readMatrix(lineSecond)
+
+            printMatrix(matrix1.add(matrix2))
+        }
+
+        fun multiplyByConstant() {
+            val matrix1 = readMatrix()
+            val constant = readConstant()
+            if (constant % 1.0 == 0.0)
+                printMatrix(matrix1.multiply(constant.toInt()))
+            else
+                printMatrix(matrix1.multiply(constant))
+        }
+
+        private fun readConstant(): Double {
+            print("Enter constant:")
+            println()
+            return readln().toDouble()
+        }
+
+        private fun printMatrix(matrix: Matrix) {
+            /**
+             * Printing all matrix elements,
+             * if matrix type is int: transform all elements to Int previously
+             */
+            println("The result is:\n" +
+                    matrix.elements.joinToString("\n") { row ->
+                        row.joinToString(" ") {
+                            if (matrix.type == Type.Int) it.toInt().toString() else it.toString()
+                        }
+                    }
             )
-        ) return false
-
-        return when {
-            isMatrixInt(rawMatrix1) && isMatrixInt(rawMatrix2) -> {
-                val matrix1 = transformToIntMatrix(rawMatrix1)
-                val matrix2 = transformToIntMatrix(rawMatrix2)
-                printMatrix(sumMatrix(matrix1, matrix2))
-            }
-            (isMatrixDouble(rawMatrix1) || isMatrixInt(rawMatrix1)) &&
-                    (isMatrixDouble(rawMatrix2) || isMatrixInt(rawMatrix2)) -> {
-                val matrix1 = transformToDoubleMatrix(rawMatrix1)
-                val matrix2 = transformToDoubleMatrix(rawMatrix2)
-                printMatrix(sumMatrix(matrix1, matrix2))
-            }
-            else -> false
         }
 
-    }
-
-    fun multiplyByConst(): Boolean {
-        val rawMatrix = readMatrix()
-        val rawConstant = readConstant()
-
-        return when {
-            isMatrixInt(rawMatrix) && isConstantInt(rawConstant) -> {
-                val matrix = transformToIntMatrix(rawMatrix)
-                val constant = rawConstant.toInt()
-                printMatrix(multiplyMatrixByConstant(matrix, constant))
-            }
-            (isMatrixDouble(rawMatrix) || isMatrixInt(rawMatrix)) &&
-                    (isConstantDouble(rawConstant) || isConstantInt(rawConstant)) -> {
-                val matrix = transformToDoubleMatrix(rawMatrix)
-                val constant = rawConstant.toDouble()
-                printMatrix(multiplyMatrixByConstant(matrix, constant))
-            }
-            else -> false
+        private fun checkMatrixSizes(
+            rowsFirst: Int,
+            columnsFirst: Int,
+            rowsSecond: Int,
+            columnsSecond: Int
+        ): Boolean {
+            return rowsFirst == rowsSecond && columnsFirst == columnsSecond
         }
     }
 
-    fun multiplyMatrices(): Boolean {
-        val rawMatrix1 = readMatrix(lineFirst)
-        val rawMatrix2 = readMatrix(lineSecond)
-
-        if (rawMatrix1.first().size != rawMatrix2.size) return false
-
-        return when {
-            isMatrixInt(rawMatrix1) && isMatrixInt(rawMatrix2) -> {
-                val matrix1 = transformToIntMatrix(rawMatrix1)
-                val matrix2 = transformToIntMatrix(rawMatrix2)
-                printMatrix(multiplyMatrix(matrix1, matrix2))
-            }
-            (isMatrixDouble(rawMatrix1) || isMatrixInt(rawMatrix1)) &&
-                    (isMatrixDouble(rawMatrix2) || isMatrixInt(rawMatrix2)) -> {
-                val matrix1 = transformToDoubleMatrix(rawMatrix1)
-                val matrix2 = transformToDoubleMatrix(rawMatrix2)
-                printMatrix(multiplyMatrix(matrix1, matrix2))
-            }
-            else -> false
-        }
+    private fun determineMatrixType(matrix: List<List<Double>>): Type {
+        matrix.forEach { row -> row.forEach { if (it % 1.0 != 0.0) return Type.Double } }
+        return Type.Int
     }
 
-    private fun readMatrix(name: String = ""): List<List<String>> {
-        print("Enter size of$name matrix: ")
-        val (rows, _) = readln().split(" ").map { it.toInt() }
-        println("Enter$name matrix:")
-        return List(rows) { readln().split(" ") }
+    fun add(other: Matrix): Matrix {
+        if (!checkMatrixSizes(
+                elements.size,
+                elements.first().size,
+                other.elements.size,
+                other.elements.first().size,
+            )
+        ) throw IllegalStateException("Matrices have different sizes")
+
+        elements = elements.mapIndexed { rowInd, row ->
+            row.mapIndexed { columnInd, it ->
+                it + other.elements[rowInd][columnInd]
+            }.toMutableList()
+        }.toMutableList()
+        return this
     }
 
-    private fun readConstant(): String {
-        print("Enter constant:")
-        println()
-        return readln()
+    fun multiply(constant: Double): Matrix {
+        elements = elements.map { row -> row.map { it * constant }.toMutableList() }.toMutableList()
+        return this
     }
 
-    private fun transformToIntMatrix(matrix: List<List<String>>): List<List<Int>> {
-        val resultMatrix = mutableListOf<MutableList<Int>>()
-        for (row in matrix) {
-            resultMatrix.add(mutableListOf())
-            for (el in row) {
-                resultMatrix[resultMatrix.lastIndex].add(el.toInt())
-            }
-        }
-        return resultMatrix
+    fun multiply(constant: Int): Matrix {
+        elements = elements.map { row -> row.map { it * constant }.toMutableList() }.toMutableList()
+        return this
     }
 
-    private fun transformToDoubleMatrix(matrix: List<List<String>>): List<List<Double>> {
-        val resultMatrix = mutableListOf<MutableList<Double>>()
-        for (row in matrix) {
-            resultMatrix.add(mutableListOf())
-            for (el in row) {
-                resultMatrix[resultMatrix.lastIndex].add(el.toDouble())
-            }
-        }
-        return resultMatrix
-    }
-
-    private fun isMatrixInt(matrix: List<List<String>>): Boolean {
-        for (row in matrix) {
-            for (el in row) {
-                if (el.toIntOrNull() == null) return false
-            }
-        }
-        return true
-    }
-
-    private fun isMatrixDouble(matrix: List<List<String>>): Boolean {
-        for (row in matrix) {
-            for (el in row) {
-                if (el.toDoubleOrNull() == null) return false
-            }
-        }
-        return true
-    }
-
-    private fun isConstantInt(constant: String): Boolean {
-        return constant.toIntOrNull() != null
-    }
-
-    private fun isConstantDouble(constant: String): Boolean {
-        return constant.toDoubleOrNull() != null
-    }
-
-    private fun checkMatrixSizes(
-        rowsFirst: Int,
-        columnsFirst: Int,
-        rowsSecond: Int,
-        columnsSecond: Int
-    ): Boolean {
-        return rowsFirst == rowsSecond && columnsFirst == columnsSecond
-    }
-
-    @JvmName("sumMatrixInt")
-    private fun sumMatrix(
-        firstMatrix: List<List<Int>>,
-        secondMatrix: List<List<Int>>
-    ): List<List<Int>> {
-        val resultMatrix = mutableListOf<MutableList<Int>>()
-        for (i in firstMatrix.indices) {
-            resultMatrix.add(mutableListOf())
-            for (j in firstMatrix.first().indices) {
-                resultMatrix[i].add(firstMatrix[i][j] + secondMatrix[i][j])
-            }
-        }
-        return resultMatrix
-    }
-
-    private fun sumMatrix(
-        firstMatrix: List<List<Double>>,
-        secondMatrix: List<List<Double>>
-    ): List<List<Double>> {
-        val resultMatrix = mutableListOf<MutableList<Double>>()
-        for (i in firstMatrix.indices) {
-            resultMatrix.add(mutableListOf())
-            for (j in firstMatrix.first().indices) {
-                resultMatrix[i].add(firstMatrix[i][j] + secondMatrix[i][j])
-            }
-        }
-        return resultMatrix
-    }
-
-    private fun <T> printMatrix(matrix: List<List<T>>?): Boolean {
-        println( "The result is:\n" +
-                (matrix?.joinToString("\n") { row ->
-                row.joinToString(" ") { it.toString() }
-            } ?: return false)
-        )
-        return true
-    }
-
-    private fun multiplyMatrixByConstant(
-        matrix: List<List<Int>>,
-        constant: Int
-    ): List<List<Int>> {
-        return matrix.map { row -> row.map { it * constant } }
-    }
-
-    private fun multiplyMatrixByConstant(
-        matrix: List<List<Double>>,
-        constant: Double
-    ): List<List<Double>> {
-        return matrix.map { row -> row.map { it * constant } }
-    }
-
-    @JvmName("multiplyMatrixInt")
-    private fun multiplyMatrix(
-        firstMatrix: List<List<Int>>,
-        secondMatrix: List<List<Int>>
-    ): List<List<Int>> {
-        val resultMatrix = mutableListOf<MutableList<Int>>()
-        for (row in firstMatrix.indices) {
-            resultMatrix.add(mutableListOf())
-            for (column in secondMatrix.first().indices) {
-                resultMatrix[row].add(0)
-                for (i in firstMatrix.first().indices) {
-                    resultMatrix[row][column] += firstMatrix[row][i] * secondMatrix[i][column]
+    fun multiply(other: Matrix): Matrix {
+        if (elements.first().size != other.elements.size)
+            throw IllegalStateException("Matrices have incompatible sizes")
+        val resultElements = mutableListOf<MutableList<Double>>()
+        for (row in 0 until rows) {
+            resultElements.add(mutableListOf())
+            for (column in 0 until other.columns) {
+                resultElements[row].add(0.0)
+                for (i in 0 until columns) {
+                    resultElements[row][column] += elements[row][i] * other.elements[i][column]
                 }
             }
         }
-        return resultMatrix
-    }
-
-    private fun multiplyMatrix(
-        firstMatrix: List<List<Double>>,
-        secondMatrix: List<List<Double>>
-    ): List<List<Double>> {
-        val resultMatrix = mutableListOf<MutableList<Double>>()
-        for (row in firstMatrix.indices) {
-            resultMatrix.add(mutableListOf())
-            for (column in secondMatrix.first().indices) {
-                resultMatrix[row].add(0.0)
-                for (i in firstMatrix.first().indices) {
-                    resultMatrix[row][column] += firstMatrix[row][i] * secondMatrix[i][column]
-                }
-            }
-        }
-        return resultMatrix
+        columns = other.columns
+        elements = resultElements
+        return this
     }
 
 }
